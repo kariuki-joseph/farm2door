38c4b2c57bf7f747a377402f29ebada41e849f54
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-12-17 12:06:48 +0300

    fix: update selected item in bottom navbar

--- app/src/main/java/com/example/farm2door/BaseActivity.java
+++ app/src/main/java/com/example/farm2door/BaseActivity.java
@@ -11,6 +11,7 @@ import com.example.farm2door.databinding.ActivityBaseBinding;
 import com.example.farm2door.fragments.AccountFragment;
 import com.example.farm2door.fragments.HomeFragment;
 import com.example.farm2door.fragments.OrdersFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public abstract class BaseActivity extends AppCompatActivity {
@@ -30,17 +31,24 @@ public abstract class BaseActivity extends AppCompatActivity {
             Fragment selectedFragment = null;
             int itemId = item.getItemId();
 
+            int selectedIndex = 0;
+
             if (itemId == R.id.home_tab) {
                 selectedFragment = new HomeFragment();
             } else if (itemId == R.id.orders_tab) {
                 selectedFragment = new OrdersFragment();
+                selectedIndex = 1;
             } else if (itemId == R.id.account_tab) {
                 selectedFragment = new AccountFragment();
+                selectedIndex = 2;
             }
 
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, selectedFragment)
                     .commit();
+
+            // update selected tab item
+            binding.bottomNavigationView.getMenu().getItem(selectedIndex).setChecked(true);
 
             return false;
         });
