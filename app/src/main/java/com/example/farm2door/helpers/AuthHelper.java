f2b2af0c535e5fb49fcf4c07db4ecf768ce06e6f
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-12-21 07:14:43 +0300

    check whether user logged in as admin

---
+++ app/src/main/java/com/example/farm2door/helpers/AuthHelper.java
@@ -0,0 +1,8 @@
+package com.example.farm2door.helpers;
+
+public class AuthHelper {
+    static final boolean IS_USER_ADMIN = true;
+    public static boolean isUserAdmin() {
+        return IS_USER_ADMIN;
+    }
+}
