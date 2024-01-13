2fdeb2678db4f7cd76db88609139912274b723a0
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-01-13 16:26:19 +0300

    Add google map location marker in placing order

--- app/src/main/java/com/example/farm2door/AddLocation.java
+++ app/src/main/java/com/example/farm2door/AddLocation.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.farm2door.databinding.ActivityAddLocationBinding;
 import com.example.farm2door.helpers.ToolBarHelper;
@@ -12,6 +13,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -23,7 +26,7 @@ public class AddLocation extends AppCompatActivity implements OnMapReadyCallback
     Polyline polyline;
     Marker draggableMarker;
 
-    LatLng INITIAL_POSITION = new LatLng(-0.4030464, 36.9721344);
+    LatLng INITIAL_POSITION = new LatLng(-0.391396,  36.933992);
     double orderLat, orderLng; // store the location of the customer making the order
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +48,11 @@ public class AddLocation extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
+        // create custom marker for the draggable marker
+        BitmapDescriptor customMarker = BitmapDescriptorFactory.fromResource(R.drawable.location_marker);
 
         // place marker on initial position
-        draggableMarker = map.addMarker(new MarkerOptions().position(INITIAL_POSITION).draggable(true));
+        draggableMarker = map.addMarker(new MarkerOptions().position(INITIAL_POSITION).icon(customMarker).draggable(true));
         // move camera to initial position and set the zoom level
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(INITIAL_POSITION, 12.0f));
 
@@ -60,6 +65,7 @@ public class AddLocation extends AppCompatActivity implements OnMapReadyCallback
         LatLng newPosition = marker.getPosition();
         orderLat = newPosition.latitude;
         orderLng = newPosition.longitude;
+        binding.tvCoords.setText(String.valueOf(orderLat)+","+String.valueOf(orderLng));
     }
 
     @Override
