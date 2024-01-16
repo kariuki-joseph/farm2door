d6e2cb7247f9ffde77e5c1a76659ded77a69ba22
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-01-16 10:31:53 +0300

    save logged in user in livedata

--- app/src/main/java/com/example/farm2door/repository/AuthRepository.java
+++ app/src/main/java/com/example/farm2door/repository/AuthRepository.java
@@ -1,14 +1,22 @@
 package com.example.farm2door.repository;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.farm2door.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class AuthRepository {
     FirebaseAuth mAuth;
+    MutableLiveData<User> userLiveData = new MutableLiveData<>();
     public AuthRepository() {
         mAuth = FirebaseAuth.getInstance();
     }
 
+    public LiveData<User> getLoggedInUser(){
+        return userLiveData;
+    }
     // login user
     public void loginUser(String email, String password, final AuthCallback callback){
         mAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
@@ -18,6 +26,10 @@ public class AuthRepository {
         });
     }
 
+    // set logged in user
+    public void setLoggedInUser(User user){
+        userLiveData.setValue(user);
+    }
     // register user
     public void registerUser(String email, String password, final AuthCallback callback){
         mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
