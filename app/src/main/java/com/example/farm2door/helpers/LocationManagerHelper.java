8d6d3787555d4866246dcf481f63f0c55fff636b
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-02-16 14:31:07 +0300

    get live location of the farmer

---
+++ app/src/main/java/com/example/farm2door/helpers/LocationManagerHelper.java
@@ -0,0 +1,2 @@
+package com.example.farm2door.helpers;public class LocationManagerHelper {
+}
---
+++ app/src/main/java/com/example/farm2door/interfaces/MyLocationListener.java
@@ -0,0 +1,7 @@
+package com.example.farm2door.helpers;
+
+import android.location.Location;
+
+public interface MyLocationListener {
+    public void OnLocationChanged(Location location);
+}
