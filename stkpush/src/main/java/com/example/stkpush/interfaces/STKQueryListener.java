5969b68ab4bd4d7a52c1bbd0076ee01396e9e550
chepkorir mercy <chepkorirmercy413@gmail.com>
2024-03-07 12:27:06 +0300

    created m-pesa

---
+++ stkpush/src/main/java/com/example/stkpush/interfaces/STKQueryListener.java
@@ -0,0 +1,14 @@
+package com.example.stkpush.interfaces;
+
+import com.example.stkpush.api.response.STKPushResponse;
+
+/**
+ * @author Fredrick Ochieng on 02/02/2018.
+ */
+
+public interface STKQueryListener {
+
+    void onResponse(STKPushResponse stkPushResponse);
+
+    void onError(Throwable throwable);
+}
