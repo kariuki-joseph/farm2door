971d406587518b97d5c8998783b32dc4bfae0e41
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-04-04 16:54:23 +0300

    fix: change request to use ngrok

--- app/src/main/java/com/example/farm2door/PredictActivity.java
+++ app/src/main/java/com/example/farm2door/PredictActivity.java
@@ -9,7 +9,11 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -21,10 +25,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PredictActivity extends AppCompatActivity {
 
     public static final String TAG = "PredictActivity";
-    public static final String PREDICTION_SERVER_URL = "http://10.42.0.1:5000/predict?duration=7";
+    public static final String PREDICTION_SERVER_URL = "https://6e1b-41-209-57-187.ngrok-free.app/predict";
     RequestQueue requestQueue;
     GraphView graphView;
     ProgressBar progressBar;
@@ -54,34 +61,46 @@ public class PredictActivity extends AppCompatActivity {
 
         graphView.addSeries(series);
 
-        StringRequest predictRequest = new StringRequest(PREDICTION_SERVER_URL, response -> {
-            // parse using JSON
-            try {
-                JSONObject responseJson = new JSONObject(response);
-                JSONArray predictions = responseJson.getJSONArray("predictions");
-                double[] predictionValues = new double[predictions.length()];
-                for (int i = 0; i < predictions.length(); i++) {
-                    predictionValues[i] = predictions.getDouble(i);
-                }
+        // data to send to prediction server
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("duration", 7);
+        // initiate POST request to prediction server
+        JsonObjectRequest predictRequest = new JsonObjectRequest(Request.Method.POST, PREDICTION_SERVER_URL, new JSONObject(requestBody),
+                (Response.Listener<JSONObject>) response -> {
+                    // parse using JSON
+                    try {
+                        JSONArray predictions = response.getJSONArray("predictions");
+                        double[] predictionValues = new double[predictions.length()];
+                        for (int i = 0; i < predictions.length(); i++) {
+                            predictionValues[i] = predictions.getDouble(i);
+                        }
 
-                // plot data points for the graph
-                DataPoint[] dataPoints = new DataPoint[predictionValues.length];
-                for (int i = 0; i < predictionValues.length; i++) {
-                    dataPoints[i] = new DataPoint(i, predictionValues[i]);
-                }
+                        // plot data points for the graph
+                        DataPoint[] dataPoints = new DataPoint[predictionValues.length];
+                        for (int i = 0; i < predictionValues.length; i++) {
+                            dataPoints[i] = new DataPoint(i, predictionValues[i]);
+                        }
 
-                series.resetData(dataPoints);
-                progressBar.setVisibility(View.GONE);
-                Toast.makeText(this, "Prediction success!", Toast.LENGTH_SHORT).show();
-            } catch (JSONException e) {
-                Toast.makeText(this, "Json Format Error", Toast.LENGTH_SHORT).show();
-                progressBar.setVisibility(View.GONE);
+                        series.resetData(dataPoints);
+                        progressBar.setVisibility(View.GONE);
+                        Toast.makeText(getApplicationContext(), "Prediction success!", Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), "Json Format Error", Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+                },
+                (Response.ErrorListener) error -> {
+                    // handle error
+                    Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                return headers;
             }
-
-        }, error -> {
-            // handle error
-            Toast.makeText(this, "Connect timeout to "+PredictActivity.PREDICTION_SERVER_URL, Toast.LENGTH_SHORT).show();
-        });
+        };
 
         requestQueue.add(predictRequest);
 
